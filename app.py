dfaaa4eaab8b3e36178cc1a625ff70847e597e32
Kurt <38019652+iiKurt@users.noreply.github.com>
2022-04-24 16:09:21 +1200

    Make it clearer what app.py is doing

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ sys.path.append(running + "/Source/") # lets us import stuff when importing from
 from Source import app, setup
 
 if "--debug" in sys.argv:
+	print("Running in debug mode...")
 	app.debug = True
 	app.env = "development"
 	setup()
 	app.run(host='0.0.0.0', port=80, debug=True)
 else:
+	print("Not running directly...")
+	print("...use \"--debug\" to run in debug mode.")
 	setup()
